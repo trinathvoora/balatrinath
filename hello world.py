5ccc9db5d197d95f8b26fdaabcebf46d2949161b
trinathvoora <88022719+trinathvoora@users.noreply.github.com>
2023-10-26 11:53:44 +0530

    python basics.py
    just for beginners to learn python

--- hello world.py
+++ hello world.py
@@ -1,5 +1,35 @@
+# python basics
 print("hello world")
+a=10
+b=20
+c=a+b
+print(type(c),id(c))
+a=100.0
+b=200.0
+c=(a+b)
+print(type(c),id(c))
+#type conversion
+print(c)
+print(int(c))
+print(bool(c))
+print(str(c))
+#write a code for the calendar 
+import calendar
+print(calendar.calendar(2023))
+print(calendar.month(2024,1))
+print("python")
+s="python"
+print((type(s),id(s)))
+s="python"
+print(s[1:4])
+#yth
+print(s[2:3])
+#t
+print(s[:])
 
+
+
+      
 def wish():
   print("happy birthday voora bala  trinath")
   
